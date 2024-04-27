66ef691ab84cc16d1b48efc86ad73180846dbfee
Micaela Tapia <mclmrls29@gmail.com>
2024-04-27 09:59:20 -0400

    Prep3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number");
+        int magicNumber = Convert.ToInt32(Console.ReadLine());
+        
+
+        //ask user for guess 
+        Console.Write("What is your guess? ");
+        int guess = Convert.ToInt32(Console.ReadLine());
+        
+        if (guess < magicNumber)
+            Console.WriteLine("Higher");
+        else if (guess > magicNumber)
+            Console.WriteLine("Lower");
+        else 
+            Console.WriteLine("You guessed it!");
+
+        // Add loop to keep guessing
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+                
+            if (guess < magicNumber)
+                Console.WriteLine("Higher");
+            else if (guess > magicNumber)
+                Console.WriteLine("Lower");
+            else 
+                Console.WriteLine("You guessed it!");
+        }
+
     }
 }
