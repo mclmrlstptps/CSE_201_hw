4f5a2e5b8e2a01b72c63a2a5a90e6637652b2573
Micaela Tapia <mclmrls29@gmail.com>
2024-04-27 09:48:40 -0400

    prep2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade percentage: ");
+        double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+        char letter;
+        if (gradePercentage >= 90)
+            letter = 'A';
+        else if (gradePercentage >= 80)
+            letter = 'B';
+        else if (gradePercentage >= 70)
+            letter = 'C';
+        else if (gradePercentage >= 60)
+            letter = 'D';
+        else
+            letter = 'F';
+
+        Console.WriteLine("Your letter grade is: " + letter);
+
+        if (gradePercentage >= 70)
+            Console.WriteLine("Congratulations! You Passed the course.");
+        else
+            Console.WriteLine("Keep working hard for next time!");
+
     }
+
 }
