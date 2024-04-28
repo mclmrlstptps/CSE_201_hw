680bff903a7f56a0eb2d15289b7dce776c634af0
Micaela Tapia <mclmrls29@gmail.com>
2024-04-27 22:10:24 -0400

    Prep4 Complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<double> numbers = new List<double>();
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            if (number == 0)
+                break;
+            numbers.Add(number);
+        }
+
+        double total = 0;
+        foreach(double number in numbers)
+        {
+            total += number;
+        }
+
+        double average = total / numbers.Count;
+        
+        double maximum = double.MinValue;
+        foreach (double number in numbers)
+        {
+            if (number > maximum)
+                maximum = number;
+        }
+
+        Console.WriteLine("The sum is: " + total);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + maximum);
     }
 }
